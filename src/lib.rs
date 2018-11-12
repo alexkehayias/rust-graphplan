5a55c6a7406f69e12ceaf525a3e1519bd21454b8
Alex Kehayias <alex.kehayias@gmail.com>
2018-11-11 19:43:14 -0800

    Generate layer from previous layer, add hashset macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,144 @@
-#![allow(dead_code)]
+#![crate_name = "graphplan"]
 use std::collections::HashSet;
 use std::iter::FromIterator;
 
 
+/// Create a **HashSet** from a list of elements. Implementation
+/// copied from the maplit library https://github.com/bluss/maplit
+///
+/// ## Example
+///
+/// ```
+/// #[macro_use] extern crate graphplan;
+/// # fn main() {
+///
+/// let set = hashset!{"a", "b"};
+/// assert!(set.contains("a"));
+/// assert!(set.contains("b"));
+/// assert!(!set.contains("c"));
+/// # }
+/// ```
+#[macro_export]
+macro_rules! hashset {
+    (@single $($x:tt)*) => (());
+    (@count $($rest:expr),*) => (<[()]>::len(&[$(hashset!(@single $rest)),*]));
+
+    ($($key:expr,)+) => { hashset!($($key),+) };
+    ($($key:expr),*) => {
+        {
+            let _cap = hashset!(@count $($key),*);
+            let mut _set = ::std::collections::HashSet::with_capacity(_cap);
+            $(
+                let _ = _set.insert($key);
+            )*
+            _set
+        }
+    };
+}
+
+
 #[derive(Hash, Eq, PartialEq, Clone, Debug)]
-struct Proposition {
+pub struct Proposition {
     name: &'static str,
+    negation: bool,
 }
 
 impl Proposition {
-    pub fn new(name: &'static str) -> Proposition {
-        Proposition {name: name}
+    pub fn from_str(name: &'static str) -> Proposition {
+        Proposition {name: name, negation: false}
+    }
+
+    pub fn negate(&mut self) -> Proposition {
+        self.negation = !self.negation;
+        self.to_owned()
+    }
+
+    pub fn is_negation(&mut self, prop: &Proposition) -> bool {
+        prop.name == self.name && prop.negation != self.negation
     }
 }
 
+#[test]
+fn propositions_can_be_negated() {
+    let mut p1 = Proposition::from_str("test");
+    assert!(false == p1.negation);
+    assert!(true == p1.negate().negation);
+    let mut p2 = Proposition::from_str("test");
+
+    {
+        assert!(p1.is_negation(&p2));
+    }
+
+    p2.negate();
+
+    {
+        assert!(false == p1.is_negation(&p2))
+    };
+}
+
+// TODO: Support HashSet<Action>, need to do the hashing based on the
+// name of the action. That would simplify a lot of the operations
+
 #[derive(Eq, PartialEq, Clone, Debug)]
-struct Action {
+pub struct Action {
     name: &'static str,
     reqs: HashSet<Proposition>,
-    outs: HashSet<Proposition>,
+    effects: HashSet<Proposition>,
 }
 
 impl Action {
-    pub fn new(name: &'static str) -> Action {
-        Action {name: name, reqs: HashSet::new(), outs: HashSet::new()}
+    pub fn new(name: &'static str, reqs: HashSet<Proposition>, effects: HashSet<Proposition>) -> Action {
+        Action {name: name, reqs: reqs, effects: effects}
     }
 }
 
-type ActionLayerData = Vec<Action>;
-type PropositionLayerData = Vec<Proposition>;
+pub type ActionLayerData = Vec<Action>;
+pub type PropositionLayerData = HashSet<Proposition>;
 
 #[derive(Eq, PartialEq, Clone, Debug)]
-enum Level {
+pub enum Layer {
     ActionLayer(ActionLayerData),
     PropositionLayer(PropositionLayerData),
 }
 
-impl Level {
-    // Unimplemented
-    // Convert a layer from one kind to another
-    fn from_level(all_actions: Vec<Action>,
-                  _all_propositions: Vec<Proposition>,
-                  level: Level) -> Level {
-        match &level {
-            Level::ActionLayer(_actions) => {
-                level.clone()
+impl Layer {
+    /// Create a new layer from another. ActionLayer returns a
+    /// PropositionLayer and PropositionLayer returns an ActionLayer
+    /// # Example
+    /// ```
+    /// #[macro_use] extern crate graphplan;
+    /// use graphplan::Layer;
+    /// let prop_layer = Layer::PropositionLayer(hashset!{});
+    /// Layer::from_layer(vec![], Option::None, prop_layer);
+    /// ```
+    pub fn from_layer(all_actions: Vec<Action>,
+                      prev_layer: Option<Layer>,
+                      layer: Layer)
+                      -> Layer {
+        match &layer {
+            Layer::ActionLayer(actions) => {
+                let mut effects = PropositionLayerData::new();
+                for a in actions {
+                    for e in a.effects.iter() {
+                        effects.insert(e.clone());
+                    }
+                }
+
+                // Add in maintenance action effects for initial props
+                if let Some(prop_layer) = prev_layer {
+                    match prop_layer {
+                        Layer::ActionLayer(_) => (),
+                        Layer::PropositionLayer(props) => {
+                            for p in props.iter() {
+                                effects.insert(p.clone());
+                            }
+                        }
+                    }
+                }
+
+                Layer::PropositionLayer(effects)
             },
-            Level::PropositionLayer(props) => {
+            Layer::PropositionLayer(props) => {
                 let props_hash = HashSet::from_iter(props.iter().cloned());
                 let mut al = ActionLayerData::new();
                 {
@@ -57,61 +149,95 @@ impl Level {
                         }
                     }
                 }
-                Level::ActionLayer(al)
+                Layer::ActionLayer(al)
             },
         }
     }
+    /// Returns a set of propositions that are mutually exclusive
+    pub fn proposition_mutexes(actions: Vec<Action>,
+                               props: HashSet<Proposition>)
+                               -> HashSet<Proposition> {
+
+        props
+    }
+
+    pub fn action_mutexes(actions: Vec<Action>,
+                          props: HashSet<Proposition>)
+                          -> Vec<Action> {
+        actions
+    }
 }
 
-struct PlanGraph {
-    levels: Vec<Level>
+pub struct PlanGraph {
+    layers: Vec<Layer>
 }
 
 impl PlanGraph {
     pub fn new() -> Self {
         PlanGraph {
-            levels: Vec::new()
+            layers: Vec::new()
         }
     }
 
-    pub fn push(&mut self, level: Level) {
-        self.levels.push(level.clone())
+    pub fn push(&mut self, layer: Layer) {
+        self.layers.push(layer.clone())
     }
 }
 
 #[test]
-fn it_works() {
+fn integration() {
     let mut pg = PlanGraph::new();
-    let p1 = Proposition::new("tired");
-    let p2 = Proposition::new("dog needs to pee");
-    let p3 = Proposition::new("caffeinated");
-    let p4 = Proposition::new("dog empty");
+    let p1 = Proposition::from_str("tired");
+    let p2 = Proposition::from_str("dog needs to pee");
+    let p3 = Proposition::from_str("caffeinated");
 
-    let mut a1 = Action::new("coffee");
-    a1.reqs.insert(p1.clone());
-    a1.outs.insert(p3.clone());
+    let a1 = Action::new(
+        "coffee",
+        hashset!{p1.clone()},
+        hashset!{p3.clone()}
+    );
 
-    let mut a2 = Action::new("walk dog");
-    a2.reqs.insert(p2.clone());
-    a2.reqs.insert(p3.clone());
-    a1.outs.insert(p4.clone());
+    let a2 = Action::new(
+        "walk dog",
+        hashset!{p2.clone(), p3.clone()},
+        hashset!{p2.clone().negate()},
+    );
 
-    let props = vec![p1.clone(), p2.clone()];
-    let prop_l1 = Level::PropositionLayer(props);
+    let all_actions = vec![a1.clone(), a2.clone()];
+
+    let props = hashset!{p1.clone(), p2.clone()};
+    let prop_l1 = Layer::PropositionLayer(props.clone());
+    let mutex_props_l1 = Layer::proposition_mutexes(
+        vec![],
+        props.clone()
+    );
 
     // Generate the action layer
-    let action_l1 = Level::from_level(
-        vec![a1.clone(), a2.clone()],
-        vec![p1.clone(), p2.clone(), p3.clone(), p4.clone()],
+    let action_l1 = Layer::from_layer(
+        all_actions.clone(),
+        Option::None,
         prop_l1.clone()
     );
-    let expected_action_l1 = Level::ActionLayer(vec![a1.clone()]);
+    let expected_action_l1 = Layer::ActionLayer(vec![a1.clone()]);
     assert!(expected_action_l1 == action_l1,
             format!("{:?} != {:?}", expected_action_l1, action_l1));
+
+    // Generate the next proposition layer
+    let prop_l2 = Layer::from_layer(
+        all_actions.clone(),
+        Some(prop_l1.clone()),
+        action_l1.clone()
+    );
+    let expected_prop_l2 = Layer::PropositionLayer(
+        hashset![p1.clone(), p2.clone(), p3.clone()]
+    );
+    assert!(expected_prop_l2 == prop_l2,
+            format!("{:?} != {:?}", expected_prop_l2, prop_l2));
 
     // Insert the layers
     pg.push(prop_l1);
     pg.push(action_l1);
+    pg.push(prop_l2);
 
-    assert!(pg.levels.len() == 2);
+    assert!(pg.layers.len() == 3);
 }
